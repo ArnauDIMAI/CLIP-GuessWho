311cb79ef8ee9875ff70693d0b831aa4a965f7e6
Arnau - DIMAI <69587457+ArnauDIMAI@users.noreply.github.com>
2021-09-10 15:15:31 +0200

    Update app_qesq_v9.py

--- app_qesq_v9.py
+++ app_qesq_v9.py
@@ -86,6 +86,7 @@ def CLIP_Process():
     ## Image Process
     st.session_state['init_data']['image_current_probs']=np.zeros((st.session_state['init_data']['n_images'],n_tokens))
     for i in range(st.session_state['init_data']['n_images']):
+        st.write(str(i)+clip_device)
         current_image_file = Load_Image(i)
         img_preprocessed = clip_transform(Image.fromarray(current_image_file).unsqueeze(0).to(clip_device))
         img_features = clip_model.encode_image(img_preprocessed)
