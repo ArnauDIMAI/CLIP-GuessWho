25d6f8e8569356e418f8b7bd94be80995991197b
Arnau - DIMAI <69587457+ArnauDIMAI@users.noreply.github.com>
2021-09-10 18:56:14 +0200

    Update app_qesq_v9.py

--- app_qesq_v9.py
+++ app_qesq_v9.py
@@ -2,7 +2,6 @@
 
 ## Used Imports
 import os
-# import io
 import zipfile
 import random
 import torch
@@ -13,8 +12,6 @@ import gc
 import cv2
 # import psutil  ## show info (cpu, memeory)
 
-# from io import BytesIO
-from PIL import Image
 from zipfile import ZipFile 
 # from streamlit import caching
 
@@ -188,14 +185,7 @@ def Select_Images_Randomly(n_images):
 def Load_Image(current_index):
     archive = zipfile.ZipFile('guess_who_images.zip', 'r')
     image_current_path=st.session_state['init_data']['image_current_paths'][current_index]
-    # image_file = cv2.imread(archive.read(image_current_path),cv2.COLOR_BGR2RGB
-    image_file = cv2.imdecode(np.frombuffer(archive.read(image_current_path), np.uint8), -1)
-    image_file=cv2.cvtColor(image_file, cv2.COLOR_BGR2RGB)
-    # st.write(type(image_file))
-    # st.write(image_file)
-    # image_file2=np.array(Image.open(BytesIO(archive.read(image_current_path))))
-    # st.write(type(image_file2))
-    # st.write(image_file2)
+    image_file=cv2.cvtColor(cv2.imdecode(np.frombuffer(archive.read(image_current_path), np.uint8), -1), cv2.COLOR_BGR2RGB)
     del image_current_path,archive
     return image_file
 
