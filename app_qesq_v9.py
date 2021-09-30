5a25086678403710cbda5d458109dba555adfaed
Arnau - DIMAI <69587457+ArnauDIMAI@users.noreply.github.com>
2021-09-30 23:43:31 +0200

    Update app_qesq_v9.py

--- app_qesq_v9.py
+++ app_qesq_v9.py
@@ -87,13 +87,11 @@ def CLIP_Process():
     for i in range(st.session_state['init_data']['n_images']):
         current_image_file = Load_Image(i)
         img_preprocessed = clip_transform(Image.fromarray(current_image_file)).unsqueeze(0).to("cpu")
-        img_features = clip_model.encode_image(img_preprocessed)
-        txt_features = clip_model.encode_text(clip_text)
         img_logits, img_logits_txt = clip_model(img_preprocessed, clip_text)
         st.session_state['init_data']['image_current_probs'][i,:]=np.round(img_logits.detach().numpy()[0],2)
         gc.collect()
         
-    del i,n_tokens,clip_model,clip_transform,clip_text,current_image_file,img_preprocessed,img_features,txt_features,img_logits,img_logits_txt
+    del i,n_tokens,clip_model,clip_transform,clip_text,current_image_file,img_preprocessed,img_logits,img_logits_txt
     gc.collect()
     
        
