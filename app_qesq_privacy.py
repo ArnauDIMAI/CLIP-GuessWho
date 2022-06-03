0265c32b0ef05463465b212575c4de3dc3dfcb55
Arnau - DIMAI <69587457+ArnauDIMAI@users.noreply.github.com>
2022-06-03 15:44:54 +0200

    Update app_qesq_privacy.py

--- app_qesq_privacy.py
+++ app_qesq_privacy.py
@@ -189,9 +189,9 @@ def Select_Images_Randomly():
         for i in image_delete:
             image_index_all.remove(i)  
             
-        print('index',image_index[0]) 
-        print('list',image_delete)
-        print('list all',image_index_all)
+        print('index0',image_index[0]) 
+        print('list0',image_delete)
+        print('list all0',image_index_all)
         current_index=1 
         while len(image_index)<st.session_state['init_data']['n_images']:  
             image_index.append(random.choice(image_index_all))  
