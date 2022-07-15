3cbb5622ef3a66f66e807ad5882735575b36d53c
Arnau - DIMAI <69587457+ArnauDIMAI@users.noreply.github.com>
2022-07-15 13:44:44 +0200

    Update app_qesq_privacy.py

--- app_qesq_privacy.py
+++ app_qesq_privacy.py
@@ -211,7 +211,7 @@ def Select_Images_Randomly():
             image_current_path=listOfFileNames[current_index]
             st.session_state['init_data']['image_current_paths'].append(image_current_path)
             current_name = os.path.basename(image_current_path)
-            current_name = current_name[:current_name.find('-')-2]
+            current_name = current_name[:current_name.find('-')-1]
             st.session_state['init_data']['current_image_names'].append(current_name)
                     
         st.session_state['init_data']['current_image_names']=np.array(st.session_state['init_data']['current_image_names'])
