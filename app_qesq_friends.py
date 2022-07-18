bb046f323d17c185c221f0060b4b524e13609240
Arnau - DIMAI <69587457+ArnauDIMAI@users.noreply.github.com>
2022-07-18 14:28:02 +0200

    Update app_qesq_friends.py

--- app_qesq_friends.py
+++ app_qesq_friends.py
@@ -194,7 +194,7 @@ def Select_Images_Randomly():
     st.session_state['init_data']['zip_file'] = "zipFile.zip"
     open(st.session_state['init_data']['zip_file'], "wb").write(current_URL_result.content)
 
-    archive = zipfile.ZipFile(st.session_state['init_data']['zip_file'], 'r')
+    archive = zipfile.ZipFile(current_URL_result.content, 'r')
     listOfFileNames = archive.namelist()     
     # listOfFileNames = find_list_elements(listOfFileElements,[])     
     image_index_all=list(range(len(listOfFileNames)))
