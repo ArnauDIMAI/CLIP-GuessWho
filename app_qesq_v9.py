88553c46ea79aaec724c851200b9f1b66ed03cea
Arnau - DIMAI <69587457+ArnauDIMAI@users.noreply.github.com>
2022-07-20 09:32:21 +0200

    Update app_qesq_v9.py

--- app_qesq_v9.py
+++ app_qesq_v9.py
@@ -257,13 +257,11 @@ def Select_Images_Randomly():
         current_name = os.path.basename(image_current_path)
         if '-' in current_name:
             current_name = current_name[:current_name.find('-')-1]
-        else:
         st.session_state['init_data']['current_image_names'].append(current_name)
                 
     st.session_state['init_data']['current_image_names']=np.array(st.session_state['init_data']['current_image_names'])
     st.session_state['init_data']['image_current_paths']=np.array(st.session_state['init_data']['image_current_paths'])
        
-
     del image_index,archive,listOfFileNames,image_index_all,current_index,image_current_path
     
     
