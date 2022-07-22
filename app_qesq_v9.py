cb2a4a5908c290113003996a6cf87f805667324b
Arnau - DIMAI <69587457+ArnauDIMAI@users.noreply.github.com>
2022-07-22 12:59:36 +0200

    Update app_qesq_v9.py

--- app_qesq_v9.py
+++ app_qesq_v9.py
@@ -101,7 +101,7 @@ def Final_Results(N_img, Current_award, Player_indicator, Win_index, Current_ima
         if np.sum(Img_discarded==0)==1:
             st.session_state['init_data']['finished_game']=True
             st.session_state['init_data']['change_player']=False
-            st.markdown("<h1 style='text-align:left; float:left; color:gray; margin-left:0px; margin-right:15px; margin-top:0px; margin-bottom:0px;'>"+Player_indicator+". THE WINNER PICTURE IS</h1><h1 style='text-align:left; float:left; color:green; margin:0px;'>"+Current_images[Win_index]+"</h1>", unsafe_allow_html=True)
+            st.markdown("<h1 style='text-align:left; float:left; color:gray; margin-left:0px; margin-right:15px; margin-top:0px; margin-bottom:0px;'>CONGRATULATIONS <color:green;>"+Player_indicator+"<color:gray>, THE WINNER PICTURE IS <color:green;>"+Current_images[Win_index]+"</h1>", unsafe_allow_html=True)
             Finsih_Game = st.button('FINISH GAME', key='Finsih_Game')
     return Current_award
 
