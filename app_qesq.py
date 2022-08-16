c7a055d89e436f190dc31b94bb19701c61958142
Arnau - DIMAI <69587457+ArnauDIMAI@users.noreply.github.com>
2022-08-16 12:03:54 +0200

    Update app_qesq.py

--- app_qesq.py
+++ app_qesq.py
@@ -262,7 +262,7 @@ def Ask_Question(Player_indicator, Win_index, Current_award):
                         st.session_state['init_data']['function_predict']=Predict_0_vs_all
                         
                     elif Selected_Question=='Do you wear EYEGLASSES?':
-                        if st.session_state['init_data']['Selected_Images_Source']=='Use Original "Guess Who" game images':   "An illlustration of a brown-haired person's face",
+                        if st.session_state['init_data']['Selected_Images_Source']=='Use Original "Guess Who" game images':
                             st.session_state['init_data']['current_querys']=["An illlustration of a person's face with eyeglasses",
                                                                         "An illlustration of a person's face with glasses",
                                                                         "An illlustration of a person's face with sunglasses",
@@ -947,8 +947,8 @@ def Main_Program():
 
         ## Type of images
         st.markdown("<h2 style='text-align:left; float:left; color:gray; margin:0px;'>Select the set of images to play with:</h2>", unsafe_allow_html=True)
-        Selected_Images_Source=st.selectbox('Choose between: Celebrities images, Original "Guess Who" game images, My friends images or Your own images (selecting a source path with your images zip file)', 
-                                                    ['Use Celeba dataset','Use Original "Guess Who" game images', 'Use friends dataset', 'Use images from specific path'],
+        Selected_Images_Source=st.selectbox('Choose between: Celebrities images, Original "Guess Who" game images or Your own images (selecting a source path with your images zip file)', 
+                                                    ['Use Celeba dataset','Use Original "Guess Who" game images', 'Use images from specific path'],
                                                     index=0, key='Selected_Images_Source', help=None)
                                                     
         ## Current options selection                                           
