ae36961ff0fc63c71f5f78c07bbc57eaea6865e8
Arnau - DIMAI <69587457+ArnauDIMAI@users.noreply.github.com>
2022-08-25 15:56:03 +0200

    Update app_qesq_v9.py

--- app_qesq_v9.py
+++ app_qesq_v9.py
@@ -1526,9 +1526,8 @@ def Main_Program():
     ## --------------- LANGUAGE ---------------
     if st.session_state['init_data']['status']==-1:
         st.markdown(Text_Inicializations_12, unsafe_allow_html=True)
-        
+	
 	col1, col2 = st.columns([1,5])
-
 	with col1:
 		Set_English=st.button("ENGLISH", key='Set_english')
 
