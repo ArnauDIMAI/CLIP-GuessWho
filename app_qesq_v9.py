c56b9db4e76c44779b94376c17571363a965d527
Arnau - DIMAI <69587457+ArnauDIMAI@users.noreply.github.com>
2023-07-25 14:46:07 +0200

    Update app_qesq_v9.py

--- app_qesq_v9.py
+++ app_qesq_v9.py
@@ -1766,7 +1766,7 @@ def Main_Program():
     if not st.session_state['init_data']['finished_game']:
     
         ## CREATE IMAGES TO SHOW
-        if st.session_state['init_data']['status']>0 and (Uploaded_File==st.session_state['init_data']['zip_file'] or st.session_state['init_data']['Selected_Images_Source']=!List_Images_Source[2]):
+        if st.session_state['init_data']['status']>0 and (Uploaded_File==st.session_state['init_data']['zip_file'] or st.session_state['init_data']['Selected_Images_Source']!=List_Images_Source[2]):
         ## if st.session_state['init_data']['status']>0:
             [st.session_state['init_data']['show_images'], st.session_state['init_data']['Showed_image_names']]=Show_images()        
 
@@ -1792,7 +1792,7 @@ def Main_Program():
             
             
         ## SHOW CURRENT IMAGES
-        if st.session_state['init_data']['status']>0 and (Uploaded_File==st.session_state['init_data']['zip_file'] or st.session_state['init_data']['Selected_Images_Source']=!List_Images_Source[2]):
+        if st.session_state['init_data']['status']>0 and (Uploaded_File==st.session_state['init_data']['zip_file'] or st.session_state['init_data']['Selected_Images_Source']!=List_Images_Source[2]):
         ## if st.session_state['init_data']['status']>0:
             st.image(st.session_state['init_data']['show_images'], use_column_width=False, caption=st.session_state['init_data']['Showed_image_names'])        
 		
